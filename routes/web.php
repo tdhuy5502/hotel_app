eccb810d1ee88787a1d0bfacf9eb61c6f4a60cf0
unknown <huytrinh109@gmail.com>
2024-09-24 23:01:28 +0700

    register

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 Route::get('/', [AdminController::class,'home']);
 
